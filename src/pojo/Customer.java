982286b70b4304b04711dcdec401c47ae8c10468
liming <kdliming@outlook.com>
2018-03-12 16:42:26 +0800

    提炼“常客积分计算”代码

--- src/pojo/Customer.java
+++ src/pojo/Customer.java
@@ -31,14 +31,7 @@ public class Customer {
         String result = " Rental Record for " + get_name() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-
-            //add frequent renter points
-            frequentRenterPoints ++ ;
-            //add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    each.getDaysRented() > 1) {
-                frequentRenterPoints ++;
-            }
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" +
@@ -51,6 +44,8 @@ public class Customer {
                 "frequent renter points";
         return result;
     }
+
+
 
 }
 
