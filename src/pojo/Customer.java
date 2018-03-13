d1aa4239c78e151df759bf4395967472f9bd32b9
liming <kdliming@outlook.com>
2018-03-13 17:53:07 +0800

    去除临时变量

--- src/pojo/Customer.java
+++ src/pojo/Customer.java
@@ -25,12 +25,10 @@ public class Customer {
 
 
     public String statement () {
-        int frequentRenterPoints = 0 ;  //常客积分
         Enumeration rentals = _rentals.elements();
         String result = " Rental Record for " + get_name() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" +
@@ -38,8 +36,8 @@ public class Customer {
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "you earned " + String.valueOf(frequentRenterPoints) +
-                "frequent renter points";
+        result += "you earned " + String.valueOf(getTotalFrequentRenterPoints()) +
+                " frequent renter points";
         return result;
     }
 
@@ -49,6 +47,16 @@ public class Customer {
         while (retals.hasMoreElements()) {
             Rental each = (Rental) retals.nextElement();
             result += each.getCharge();
+        }
+        return  result;
+    }
+
+    private int getTotalFrequentRenterPoints () {
+        int result = 0 ;
+        Enumeration retals = _rentals.elements();
+        while (retals.hasMoreElements()) {
+            Rental each = (Rental) retals.nextElement();
+            result += each.getFrequentRenterPoints();
         }
         return  result;
     }
