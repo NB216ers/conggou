cc090fbe058f38a718d3d25deac04a6231c54d0a
liming <kdliming@outlook.com>
2018-03-16 16:48:13 +0800

    拆解switch函数，迁移Movie中getCharge和getFrequentRenterPoints方法

--- src/pojo/NewReleasePrice.java
+++ src/pojo/NewReleasePrice.java
@@ -1,8 +1,19 @@
 package pojo;
 
 public class NewReleasePrice extends Price {
+
     @Override
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    double getCharge ( int daysRented) {
+        return  daysRented * 3 ;
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+
+        return (daysRented > 1) ? 2 : 1 ;
+    }
+
 }
--- src/pojo/RegularPrice.java
+++ src/pojo/RegularPrice.java
@@ -1,8 +1,17 @@
 package pojo;
 
 public class RegularPrice extends Price {
+
     @Override
     int getPriceCode() {
         return Movie.REGULAR;
+    }
+
+    double getCharge(int daysRented){
+        double result = 2 ;
+        if (daysRented > 2) {
+            result += (daysRented - 2) * 1.5;
+        }
+        return result ;
     }
 }
